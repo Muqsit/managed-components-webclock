cbcdbbaae544f5b630a10ca40110f471670aa483
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2023-04-18 00:38:54 +0500

    Evaluate Date within the MC
    This evaluates date on the MC's side. Prior to this change, the following snippet would be sent to the client:
    "console.log(new Date())"
    
    After this change, the following snippet is sent to the client:
    "console.log('2023-04-17T19:35:24.545Z')"

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { ComponentSettings, Manager } from '@managed-components/types'
 
 export default async function (manager: Manager, _settings: ComponentSettings) {
   manager.addEventListener('pageview', event => {
-    event.client.execute('console.log(new Date())')
+    event.client.execute(`console.log('${new Date().toISOString()}')`)
   })
 }
